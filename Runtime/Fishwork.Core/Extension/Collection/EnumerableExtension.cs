a91a03ff78ec0f90917970ccb8de49ea0842be09
CodeNofish <1980114953@qq.com>
2024-12-23 20:49:58 +0800

    update

--- Runtime/Fishwork.Core/Extension/Collection/EnumerableExtension.cs
+++ Runtime/Fishwork.Core/Extension/Collection/EnumerableExtension.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace Fishwork.Core {
 
@@ -8,12 +10,24 @@ namespace Fishwork.Core {
     /// <summary>
     /// 获取IEnumerable元素数量，优先使用非枚举方法
     /// </summary>
-    public static int GetCount<T>(this IEnumerable<T> source) {
-      return source switch {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int GetCount<T>(this IEnumerable<T> sequence) {
+      return sequence switch {
         ICollection<T> collection => collection.Count,
         ICollection collection2 => collection2.Count,
-        _ => source.Count()
+        _ => sequence.Count()
       };
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void Do<T>(this IEnumerable<T> sequence, Action<T> action) {
+      if (sequence is null) return;
+      foreach (var item in sequence) action(item);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void DoIf<T>(this IEnumerable<T> sequence, Func<T, bool> condition, Action<T> action) {
+      sequence?.Where(condition).Do(action);
     }
   }
 
